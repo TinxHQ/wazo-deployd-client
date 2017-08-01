c2e96517732e9d4af5c6cff48cd474121288ef0c
Anthony Delaby <pro@anthonydelaby.me>
2017-08-01 19:16:12 +0200

    Add command class to raise custom exceptions
    Create `DeploydCommand` custom command class in order to override
    `raise_from_response` to raise custom exceptions.

---
+++ wazo_deployd_client/command.py
@@ -0,0 +1,21 @@
+# Copyright 2017 The Wazo Authors  (see AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
+from xivo_lib_rest_client.command import RESTCommand
+
+from .exceptions import DeploydError
+from .exceptions import DeploydServiceUnavailable
+from .exceptions import InvalidDeploydError
+
+
+class DeploydCommand(RESTCommand):
+
+    @staticmethod
+    def raise_from_response(response):
+        if response.status_code == 503:
+            raise DeploydServiceUnavailable(response)
+
+        try:
+            raise DeploydError(response)
+        except InvalidDeploydError:
+            RESTCommand.raise_from_response(response)
