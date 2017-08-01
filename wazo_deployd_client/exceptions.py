aa8223fed8647a0738ac5ae9567176f0c52ccbae
Anthony Delaby <pro@anthonydelaby.me>
2017-08-01 19:13:51 +0200

    Add custom exceptions to deal with deployd
    Add `DeploydError`, `DeploydServiceUnavailable` and
    `InvalidDeploydError` exceptions.

---
+++ wazo_deployd_client/exceptions.py
@@ -0,0 +1,33 @@
+# Copyright 2017 The Wazo Authors  (see AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
+from requests import HTTPError
+
+
+class DeploydError(HTTPError):
+
+    def __init__(self, response):
+        try:
+            body = response.json()
+        except ValueError:
+            raise InvalidDeploydError()
+
+        self.status_code = response.status_code
+        try:
+            self.message = body['message']
+            self.error_id = body['error_id']
+            self.details = body['details']
+            self.timestamp = body['timestamp']
+        except KeyError:
+            raise InvalidDeploydError()
+
+        exception_message = '{e.message}: {e.details}'.format(e=self)
+        super(DeploydError, self).__init__(exception_message, response=response)
+
+
+class DeploydServiceUnavailable(DeploydError):
+    pass
+
+
+class InvalidDeploydError(Exception):
+    pass
