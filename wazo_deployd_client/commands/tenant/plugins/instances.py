7ce007f4f61ad7eff5296da89e45722ae9b1c017
Anthony Delaby <pro@anthonydelaby.me>
2017-08-09 18:24:26 +0200

    Make instance command a tenant plugin
    Move `instances.py` to `commands/tenants/plugins` subpackage.

--- wazo_deployd_client/commands/tenant/plugins/instances.py
+++ wazo_deployd_client/commands/tenant/plugins/instances.py
@@ -11,11 +11,11 @@ class InstancesCommand(DeploydCommand):
     resource = 'tenants'
     _headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
 
-    def list_instances_for_tenant(self, tenant_uuid, provider_uuid=None):
+    def list(self, provider_uuid=None):
         if provider_uuid:
-            url = self._provider_instances_all_url(tenant_uuid, provider_uuid)
+            url = self._provider_instances_all_url(provider_uuid)
         else:
-            url = self._instances_all_url(tenant_uuid)
+            url = self._instances_all_url()
 
         response = self.session.get(
             url,
@@ -26,11 +26,11 @@ class InstancesCommand(DeploydCommand):
 
         return response.json()
 
-    def _create_instance(self, tenant_uuid, instance_data, provider_uuid=None):
+    def _create_instance(self, instance_data, provider_uuid=None):
         if provider_uuid:
-            url = self._provider_instances_all_url(tenant_uuid, provider_uuid)
+            url = self._provider_instances_all_url(provider_uuid)
         else:
-            url = self._instances_all_url(tenant_uuid)
+            url = self._instances_all_url()
 
         response = self.session.post(
             url,
@@ -42,15 +42,15 @@ class InstancesCommand(DeploydCommand):
 
         return response.json()
 
-    def register_instance_for_tenant(self, tenant_uuid, instance_data):
-        return self._create_instance(tenant_uuid, instance_data)
+    def register(self, instance_data):
+        return self._create_instance(instance_data)
 
-    def create_instance_for_tenant(self, tenant_uuid, provider_uuid, instance_data):
-        return self._create_instance(tenant_uuid, instance_data, provider_uuid=provider_uuid)
+    def create(self, provider_uuid, instance_data):
+        return self._create_instance(instance_data, provider_uuid=provider_uuid)
 
-    def get_instance_for_tenant(self, tenant_uuid, instance_uuid):
+    def get(self, instance_uuid):
         response = self.session.get(
-            self._instances_one_url(tenant_uuid, instance_uuid),
+            self._instances_one_url(instance_uuid),
             headers=self._headers,
         )
         if response.status_code != 200:
@@ -58,9 +58,9 @@ class InstancesCommand(DeploydCommand):
 
         return response.json()
 
-    def update_instance_for_tenant(self, tenant_uuid, instance_uuid, instance_data):
+    def update(self, instance_uuid, instance_data):
         response = self.session.put(
-            self._instances_one_url(tenant_uuid, instance_uuid),
+            self._instances_one_url(instance_uuid),
             data=json.dumps(instance_data),
             headers=self._headers,
         )
@@ -69,15 +69,14 @@ class InstancesCommand(DeploydCommand):
 
         return response.json()
 
-    def _delete_instance(self, tenant_uuid, instance_uuid, provider_uuid=None):
+    def _delete_instance(self, instance_uuid, provider_uuid=None):
         if provider_uuid:
             url = self._provider_instances_one_url(
-                tenant_uuid,
                 instance_uuid,
                 provider_uuid,
             )
         else:
-            url = self._instances_one_url(tenant_uuid, instance_uuid)
+            url = self._instances_one_url(instance_uuid)
 
         response = self.session.delete(
             url,
@@ -86,33 +85,33 @@ class InstancesCommand(DeploydCommand):
         if response.status_code != 204:
             self.raise_from_response(response)
 
-    def unregister_instance_for_tenant(self, tenant_uuid, instance_uuid):
-        return self._delete_instance(tenant_uuid, instance_uuid)
+    def unregister(self, instance_uuid):
+        return self._delete_instance(instance_uuid)
 
-    def delete_instance_for_tenant(self, tenant_uuid, provider_uuid, instance_uuid):
-        return self._delete_instance(tenant_uuid, instance_uuid, provider_uuid=provider_uuid)
+    def delete(self, provider_uuid, instance_uuid):
+        return self._delete_instance(instance_uuid, provider_uuid=provider_uuid)
 
-    def _instances_all_url(self, tenant_uuid):
+    def _instances_all_url(self):
         return '{base_url}/{tenant_uuid}/instances'.format(
             base_url=self.base_url,
-            tenant_uuid=tenant_uuid,
+            tenant_uuid=self.tenant_uuid,
         )
 
-    def _instances_one_url(self, tenant_uuid, instance_uuid):
+    def _instances_one_url(self, instance_uuid):
         return '{base_url}/{instance_uuid}'.format(
-            base_url=self._instances_all_url(tenant_uuid),
+            base_url=self._instances_all_url(),
             instance_uuid=instance_uuid,
         )
 
-    def _provider_instances_all_url(self, tenant_uuid, provider_uuid):
+    def _provider_instances_all_url(self, provider_uuid):
         return '{base_url}/{tenant_uuid}/providers/{provider_uuid}/instances'.format(
             base_url=self.base_url,
-            tenant_uuid=tenant_uuid,
+            tenant_uuid=self.tenant_uuid,
             provider_uuid=provider_uuid,
         )
 
-    def _provider_instances_one_url(self, tenant_uuid, instance_uuid, provider_uuid):
+    def _provider_instances_one_url(self, instance_uuid, provider_uuid):
         return '{base_url}/{instance_uuid}'.format(
-            base_url=self._provider_instances_all_url(tenant_uuid, provider_uuid),
+            base_url=self._provider_instances_all_url(provider_uuid),
             instance_uuid=instance_uuid,
         )
