cdb3dc65059888447665f16a7a473e92713fc200
Francois Blackburn <fblackburn@wazo.community>
2019-02-13 08:44:58 -0500

    update SPDX-License-Identifier

--- wazo_deployd_client/__init__.py
+++ wazo_deployd_client/__init__.py
@@ -1,4 +1,4 @@
 # Copyright 2017 The Wazo Authors  (see AUTHORS file)
-# SPDX-License-Identifier: GPL-3.0+
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 from wazo_deployd_client.client import DeploydClient as Client
