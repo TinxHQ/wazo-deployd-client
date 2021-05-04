48393434905c2f9e2f33f7d8a031c36437a6e4f7
Francois Blackburn <fblackburn@wazo.community>
2021-05-04 11:31:48 -0400

    fix flake8 issues

--- wazo_deployd_client/__init__.py
+++ wazo_deployd_client/__init__.py
@@ -1,4 +1,8 @@
-# Copyright 2017 The Wazo Authors  (see AUTHORS file)
+# Copyright 2017-2021 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from wazo_deployd_client.client import DeploydClient as Client
+
+__all__ = [
+    'Client',
+]
